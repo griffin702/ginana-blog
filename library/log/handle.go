8ded14ea49caf6859c7700490d7a4db21e0275eb
griffin702 <117976509@qq.com>
2020-04-08 19:30:06 +0800

    添加合理的layout以及日志输出修改

--- library/log/handle.go
+++ library/log/handle.go
@@ -39,7 +39,7 @@ type Loggers []Logger
 func (h Loggers) GetOutFile() (out io.Writer) {
 	var wr []io.Writer
 	for _, l := range h {
-		if o := l.GetOutFile(); !l.isStdOut() && o != nil {
+		if o := l.GetOutFile(); o != nil {
 			wr = append(wr, o)
 		}
 	}
