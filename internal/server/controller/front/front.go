93f96ee8888897b5758fea18afcc1abfefc5510f
griffin702 <117976509@qq.com>
2020-04-27 00:07:39 +0800

    options

--- internal/server/controller/front/front.go
+++ internal/server/controller/front/front.go
@@ -1,8 +1,10 @@
 package front
 
-//func (c *CFront) BeforeActivation(b mvc.BeforeActivation) {
-//
-//}
+import "github.com/kataras/iris/v12/mvc"
+
+func (c *CFront) BeforeActivation(b mvc.BeforeActivation) {
+	b.Handle("GET", "/about.html", "GetAbout")
+}
 
 func (c *CFront) Get() (err error) {
 	resp, err := c.Svc.GetArticles(c.Pager)
@@ -12,5 +14,12 @@ func (c *CFront) Get() (err error) {
 	c.Ctx.ViewData("data", resp)
 	c.setHeadMetas("首页")
 	c.Ctx.View("front/index.html")
+	return
+}
+
+func (c *CFront) GetAbout() (err error) {
+	c.DisableRight = true
+	c.setHeadMetas("关于我")
+	c.Ctx.View("front/about.html")
 	return
 }
