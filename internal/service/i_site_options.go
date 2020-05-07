fa5ceca0b1b3495ba1fdfea47bb586be371b75ba
griffin702 <117976509@qq.com>
2020-05-07 23:48:40 +0800

    代码整理，bug修复

--- internal/service/i_site_options.go
+++ internal/service/i_site_options.go
@@ -36,19 +36,18 @@ func (s *service) UpdateSiteOptions(req *model.Option) (err error) {
 	if err != nil {
 		return s.hm.GetMessage(1001, err)
 	}
-	key := reflect.TypeOf(options).Elem()
-	value := reflect.ValueOf(options).Elem()
-	reqKey := reflect.TypeOf(req).Elem()
-	reqValue := reflect.ValueOf(req).Elem()
+	ot := reflect.TypeOf(options).Elem()
+	ov := reflect.ValueOf(options).Elem()
+	rt := reflect.TypeOf(req).Elem()
+	rv := reflect.ValueOf(req).Elem()
 	tx := s.db.Begin()
-	for i := 0; i < key.NumField(); i++ {
-		name := key.Field(i).Name
-		for j := 0; j < reqKey.NumField(); j++ {
-			v := reqValue.Field(j).Interface()
-			if name == reqKey.Field(j).Name && value.Field(i).Interface() != v {
-				opt := new(model.Options)
-				err = s.db.Model(opt).Where("name = ?", name).Update("value", v).Error
-				if err != nil {
+	for i := 0; i < ot.NumField(); i++ {
+		name := ot.Field(i).Name
+		for j := 0; j < rt.NumField(); j++ {
+			value := rv.Field(j).Interface()
+			if name == rt.Field(j).Name && ov.Field(i).Interface() != value {
+				if err = s.db.Model(new(model.Options)).Where("name = ?", name).
+					Update("value", value).Error; err != nil {
 					tx.Rollback()
 					return s.hm.GetMessage(1003, err)
 				}
