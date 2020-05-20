362e7c01b77b769141e781dadd69e52d67b1cde9
griffin702 <117976509@qq.com>
2020-05-20 12:13:11 +0800

    bug

--- internal/controller/api/login.go
+++ internal/controller/api/login.go
@@ -139,7 +139,7 @@ func (c *CApiLogin) PostRegister() {
 		return
 	}
 	c.setToken(user)
-	log.Infof("userId: %d, username: %s, 登录成功", user.ID, user.Username)
+	//log.Infof("userId: %d, username: %s, 登录成功", user.ID, user.Username)
 	c.Ctx.JSON(c.JsonPlus(true, c.Hm.GetMessage(0, "注册并登陆成功")))
 }
 
